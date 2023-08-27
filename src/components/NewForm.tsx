e002cfde752c6e7858db8392d40e2f0d5a23da6e
Rishal <Rishal.mhd009@gmail.com>
2023-08-27 18:44:56 +0530

    uploads

--- src/components/NewForm.tsx
+++ src/components/NewForm.tsx
@@ -12,6 +12,7 @@ import { NavLink } from "react-router-dom";
 import AddIcon from "@mui/icons-material/Add";
 import BackupIcon from "@mui/icons-material/Backup";
 import "./NewForm.css";
+import axios from "axios";
 
 function NewForm() {
   const [title, setTitle] = useState<string>("");
@@ -19,22 +20,58 @@ function NewForm() {
   const [time, setTime] = useState<string>("");
   const [description, setDescription] = useState<string>("");
   const [imagePreview, setImagePreview] = useState<string>("");
-const [pdfNamePreview, setPdfNamePreview] = useState<string>('')
+  const [pdfNamePreview, setPdfNamePreview] = useState<string>("");
+  const [image, setImage] = useState<File | null>(null);
+  const [pdf, setPdf] = useState<File | null>(null);
+  const [imageNameFromServer, setImageNameFromServer] = useState<string>("");
+  const [pdfNameFromServer, setPdfNameFromServer] = useState<string>("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (image) {
+      const formData = new FormData();
+      formData.append("image", image);
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/upload-image",
+          formData
+        );
+        // console.log("Image uploaded:", response.data);
+        setImageNameFromServer(response.data);
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+    }
+
+    if (pdf) {
+      const formData = new FormData();
+      formData.append("image", pdf);
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/upload-pdf",
+          formData
+        );
+        // console.log("Image uploaded:", response.data);
+        setPdfNameFromServer(response.data);
+      } catch (error) {
+        console.error("Error uploading pdf:", error);
+      }
+    }
   };
 
   const imageUploadHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files && files.length > 0) {
       const file = files[0];
+      // console.log(file, "reareaerare");
+      setImage(file);
       const reader = new FileReader();
 
       reader.onload = (e) => {
         const imagePreviewURL = e.target?.result as string;
         // console.log("Preview URL:", imagePreviewURL);
-        setImagePreview(imagePreviewURL)
+        setImagePreview(imagePreviewURL);
       };
 
       reader.readAsDataURL(file);
@@ -48,7 +85,8 @@ const [pdfNamePreview, setPdfNamePreview] = useState<string>('')
     if (files && files.length > 0) {
       const file = files[0];
       console.log(file);
-      setPdfNamePreview(file.name)
+      setPdf(file);
+      setPdfNamePreview(file.name);
     } else {
       console.log("No file selected");
     }
@@ -147,9 +185,7 @@ const [pdfNamePreview, setPdfNamePreview] = useState<string>('')
 
             {/* pdf input */}
             <div className="pdf-input">
-              {pdfNamePreview && (
-                <Typography>{pdfNamePreview}</Typography>
-              )}
+              {pdfNamePreview && <Typography>{pdfNamePreview}</Typography>}
               <label htmlFor="pdfInput">
                 <input
                   type="file"
