41f71a112c91c0ff851d5aea5d41eff30d2c87a1
Rishal <Rishal.mhd009@gmail.com>
2023-08-27 15:40:01 +0530

    input form

--- src/components/NewForm.tsx
+++ src/components/NewForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   Button,
@@ -14,32 +14,119 @@ import BackupIcon from "@mui/icons-material/Backup";
 import "./NewForm.css";
 
 function NewForm() {
+  const [title, setTitle] = useState<string>("");
+  const [author, setAuthor] = useState<string>("");
+  const [time, setTime] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent) =>{
+    e.preventDefault()
+
+  }
+
+const imageUploadHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const file = e.target.files
+  console.log(file);
+}
+const pdfUploadHandle = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const file = e.target.files
+  console.log(file);
+}
+
+
   return (
     <Container sx={{ marginTop: "40px" }}>
-      <Grid container>
-        <Grid item xs={12} md={5}>
-          <NavLink to="/">
-            <Button
-              variant="outlined"
-              sx={{ marginBottom: "20px", marginLeft: "10px" }}
-            >
-              <KeyboardArrowLeftIcon />
-              Back to Home
-            </Button>
-          </NavLink>
-          <Box width="25%">
-            <div className="add-photo">
-              <label htmlFor="bookCoverInput">
+      <form onSubmit={handleSubmit} encType="multipart/form-data">
+        <Grid container>
+          <Grid item xs={12} md={5}>
+            <NavLink to="/">
+              <Button
+                variant="outlined"
+                sx={{ marginBottom: "20px", marginLeft: "10px" }}
+              >
+                <KeyboardArrowLeftIcon />
+                Back to Home
+              </Button>
+            </NavLink>
+            <Box width="25%">
+              <div className="add-photo">
+                <label htmlFor="bookCoverInput">
+                  <input
+                    type="file"
+                    id="bookCoverInput"
+                    accept="image/*"
+                    style={{ display: "none" }}
+                    onChange={imageUploadHandle}
+                  />
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      cursor: "pointer",
+                    }}
+                  >
+                    <AddIcon />
+                    <Typography
+                      sx={{ textDecoration: "underline", color: "darkblue" }}
+                    >
+                      Add a book Cover
+                    </Typography>
+                  </div>
+                </label>
+              </div>
+            </Box>
+          </Grid>
+
+          {/* inputs sections */}
+          <Grid item xs={12} md={7} sx={{ marginTop: "50px" }}>
+            <TextField
+              fullWidth
+              label="Book Title"
+              placeholder="Enter book title"
+              margin="normal"
+              sx={{ marginBottom: 2 }}
+              value={title}
+              onChange={(e)=> setTitle(e.target.value)}
+            />
+            <div style={{ display: "flex", marginBottom: "18px" }}>
+              <TextField
+                fullWidth
+                label="Author"
+                placeholder="Enter author name"
+                style={{ marginRight: "16px" }}
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+              />
+              <TextField
+                fullWidth
+                label="Book read time"
+                placeholder="Add time in mins"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+              />
+            </div>
+            <TextField
+              fullWidth
+              label="Description"
+              placeholder="Enter book description"
+              multiline
+              rows={4}
+              margin="normal"
+              sx={{ marginBottom: 2 }}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+
+            {/* pdf input */}
+            <div className="pdf-input">
+              <label htmlFor="pdfInput">
                 <input
                   type="file"
-                  id="bookCoverInput"
-                  accept="image/*"
-                  style={{ display: "none" }}
-                  onChange={(e) => {
-                    // Handle file selection here
-                    //   const selectedFile = e.target.files[0];
-                    //   console.log("Selected file:", selectedFile);
-                  }}
+                  id="pdfInput"
+                  accept=".pdf" // Allow only PDF files
+                  style={{ display: "none" }} // Hide the input visually
+                  onChange={pdfUploadHandle}
                 />
                 <div
                   style={{
@@ -49,87 +136,25 @@ function NewForm() {
                     cursor: "pointer",
                   }}
                 >
-                  <AddIcon />
+                  <BackupIcon sx={{ color: "darkblue" }} />
                   <Typography
-                    sx={{ textDecoration: "underline", color: "darkblue" }}
+                    sx={{
+                      textDecoration: "underline",
+                      color: "darkblue",
+                      cursor: "pointer",
+                    }}
                   >
-                    Add a book Cover
+                    Add a PDF File
                   </Typography>
                 </div>
               </label>
             </div>
-          </Box>
+            <Button sx={{marginTop: '30px', marginLeft:"20px"}} type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </Grid>
         </Grid>
-
-        {/* inputs sections */}
-        <Grid item xs={12} md={7} sx={{ marginTop: "50px" }}>
-          <TextField
-            fullWidth
-            label="Book Title"
-            placeholder="Enter book title"
-            margin="normal"
-            sx={{ marginBottom: 5 }}
-          />
-          <div style={{ display: "flex", marginBottom: "18px" }}>
-            <TextField
-              fullWidth
-              label="Author"
-              placeholder="Enter author name"
-              style={{ marginRight: "16px" }}
-            />
-            <TextField
-              fullWidth
-              label="Book read time"
-              placeholder="Add time in mins"
-            />
-          </div>
-          <TextField
-            fullWidth
-            label="Description"
-            placeholder="Enter book description"
-            multiline
-            rows={4}
-            margin="normal"
-            sx={{ marginBottom: 5 }}
-          />
-
-          {/* pdf input */}
-          <div className="pdf-input">
-            <label htmlFor="pdfInput">
-              <input
-                type="file"
-                id="pdfInput"
-                accept=".pdf" // Allow only PDF files
-                style={{ display: "none" }} // Hide the input visually
-                onChange={(e) => {
-                  // Handle PDF file selection here
-                  //   const selectedPDF = e.target.files[0];
-                  //   console.log("Selected PDF:", selectedPDF);
-                }}
-              />
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  cursor: "pointer",
-                }}
-              >
-                <BackupIcon sx={{ color: "darkblue" }} />
-                <Typography
-                  sx={{
-                    textDecoration: "underline",
-                    color: "darkblue",
-                    cursor: "pointer",
-                  }}
-                >
-                  Add a PDF File
-                </Typography>
-              </div>
-            </label>
-          </div>
-        </Grid>
-      </Grid>
+      </form>
     </Container>
   );
 }
