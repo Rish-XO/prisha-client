7d1d06f42c2ce0d6b3c7c10df58f0daa3fa1b1bf
Rishal <Rishal.mhd009@gmail.com>
2023-08-28 21:19:23 +0530

    final

--- src/components/ShowBook.tsx
+++ src/components/ShowBook.tsx
@@ -14,27 +14,39 @@ import { NavLink, useParams } from "react-router-dom";
 import React, { useState } from "react";
 import { trpc } from "../lib/trpc";
 import { pdfjs, Document, Page } from "react-pdf";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
+import "./ShowBook.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ShowBook() {
   const [viewPdf, setViewPdf] = useState(false);
-  const [numPages, setNumPages] = useState<number>();
+  const [numPages, setNumPages] = useState<number>(0);
   const [pageNumber, setPageNumber] = useState<number>(1);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
   }
 
+  const openPdf = () => {
+    setViewPdf(true);
+  };
 
-  const openPdf = () =>{
-    setViewPdf(true)
-  }
+  const closePdf = () => {
+    setViewPdf(false);
+  };
 
-  const closePdf = () =>{
-    setViewPdf(false)
-  }
+  const goToPreviousPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  };
 
   const { id } = useParams();
   console.log(id, "id from params");
@@ -47,23 +59,31 @@ function ShowBook() {
     <Container sx={{ marginTop: "40px" }}>
       {/* rendering the pdf */}
 
-      {book &&  (
-      <Dialog open={viewPdf} onClose={closePdf} maxWidth="md" fullWidth>
-        <IconButton
-          aria-label="close"
-          onClick={closePdf}
-          sx={{ position: "absolute", right: 8, top: 8 }}
-        >
-          <CloseIcon />
-        </IconButton>
-        <DialogContent>
-
-        <Document file={book.pdf} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
-        </Document>
-        </DialogContent>
-      </Dialog>
+      {book && (
+        <Dialog open={viewPdf} onClose={closePdf} maxWidth="md" fullWidth>
+          <IconButton
+            aria-label="close"
+            onClick={closePdf}
+            sx={{ position: "absolute", right: 8, top: 8 }}
+          >
+            <CloseIcon />
+          </IconButton>
+          <DialogContent>
+          <Box sx={{ marginTop: "20px", textAlign: "center" }}>
+              <Button onClick={goToPreviousPage}>Previous Page</Button>{" "}
+              <span>
+                Page {pageNumber} of {numPages}
+              </span>{" "}
+              <Button onClick={goToNextPage}>Next Page</Button>
+            </Box>
+            <Document file={book.pdf} onLoadSuccess={onDocumentLoadSuccess} >
+              <Page pageNumber={pageNumber} className="pdf"/>
+            </Document>
+            
+          </DialogContent>
+        </Dialog>
       )}
+
       {book && (
         <Grid container>
           <Grid item xs={12} md={5}>
@@ -76,7 +96,7 @@ function ShowBook() {
             <Box width="25%">
               <img
                 src={book.image}
-                alt="book image"
+                alt=""
                 style={{
                   maxWidth: "600px",
                   height: "500px",
@@ -110,7 +130,9 @@ function ShowBook() {
               <Rating></Rating>
             </Box>
             <Box sx={{ marginTop: "40px" }}>
-              <Button variant="contained" onClick={openPdf}>Read this Book</Button>
+              <Button variant="contained" onClick={openPdf}>
+                Read this Book
+              </Button>
             </Box>
           </Grid>
         </Grid>
