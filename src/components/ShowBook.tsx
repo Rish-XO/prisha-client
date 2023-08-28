221de48d3a3d3d69111b22f166b7b4a77557cd32
Rishal <Rishal.mhd009@gmail.com>
2023-08-28 19:11:55 +0530

    starta

--- src/components/ShowBook.tsx
+++ src/components/ShowBook.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Rating,
+  Typography,
+} from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { NavLink, useParams } from "react-router-dom";
 import React from "react";
@@ -46,7 +53,28 @@ function ShowBook() {
             </Box>
           </Grid>
           <Grid item xs={12} md={7}>
-            <Typography>{book.title}</Typography>
+            <Typography
+              variant="h5"
+              fontWeight={700}
+              sx={{ color: "darkblue", marginTop: "50px" }}
+            >
+              {book.title}
+            </Typography>
+            <Typography sx={{ marginTop: "10px" }} variant="subtitle2">
+              {book.author}
+            </Typography>
+            <Typography sx={{ marginTop: "10px" }} variant="subtitle2">
+              Book read time: {book.time} mins
+            </Typography>
+            <Typography variant="h6" fontWeight={500} sx={{ marginTop: "10px" }}>
+              {book.description} 
+            </Typography>
+            <Box sx={{marginTop: "40px"}}>
+              <Rating></Rating>
+            </Box>
+            <Box sx={{marginTop: "40px"}}>
+              <Button variant="contained">Read this Book</Button>
+            </Box>
           </Grid>
         </Grid>
       )}
