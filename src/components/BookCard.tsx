79a2758ad94ec370cc137871fa435e4b5b313bcd
Rishal <Rishal.mhd009@gmail.com>
2023-08-28 17:57:52 +0530

    show

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -49,7 +49,7 @@ function BookCard() {
                 }}
                 component="img"
                 height="180"
-                image="https://images.unsplash.com/photo-1584448141569-69f342da535c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2541&q=80"
+                image={item.image}
               />
             </CardActionArea>
           </Card>
--- src/components/ShowBook.tsx
+++ src/components/ShowBook.tsx
@@ -1,18 +1,26 @@
-import { Box, Button, Container, Grid } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { NavLink, useParams } from "react-router-dom";
 import React from "react";
 import { trpc } from "../lib/trpc";
 
+// interface Book {
+//   author: string;
+//   book_id: string;
+//   description: string;
+//   image: string;
+//   pdf: string;
+//   time: string;
+//   title: string;
+// }
+
 function ShowBook() {
   const { id } = useParams();
-  console.log(id);
-  if (id) {
-    const ID = { id: id };
-    const getABookquery = trpc.getABook.useQuery(ID);
-    const data = getABookquery.data;
-    console.log(data);
-  }
+  console.log(id, "id from params");
+
+  const book: any = trpc.getABook.useQuery({ id: id ?? "" }).data;
+
+  console.log(book, "data from trpc");
 
   return (
     <Container sx={{ marginTop: "40px" }}>
@@ -25,22 +33,21 @@ function ShowBook() {
             </Button>
           </NavLink>
           <Box width="25%">
-            <img
-              src="https://images.unsplash.com/photo-1584448141569-69f342da535c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2541&q=80"
-              alt=""
-              style={{
-                maxWidth: "600px",
-                height: "500px",
-                objectFit: "contain",
-              }}
-            />
+            {book && (
+              <img
+                src={book.image}
+                alt=""
+                style={{
+                  maxWidth: "600px",
+                  height: "500px",
+                  objectFit: "contain",
+                }}
+              />
+            )}
           </Box>
         </Grid>
         <Grid item xs={12} md={7}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque sed
-          nulla architecto ex a explicabo ut, praesentium debitis dolorem
-          repellendus cupiditate tenetur minima obcaecati ad omnis. Libero saepe
-          illo architecto.
+          <Typography>balls</Typography>
         </Grid>
       </Grid>
     </Container>
